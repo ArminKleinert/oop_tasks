bb011c978fab9b5b0f2b078dfd794b7131fc9770
ArminKleinert <armin.kleinert@t-online.de>
2020-06-13 14:54:49 +0200

    Docs.

--- task4/task.py
+++ task4/task.py
@@ -3,6 +3,9 @@ import operator
 import math
 import time
 
+"""
+Helper function for the whole file
+"""
 def is_sorted(op, lst):
     if len(lst) <= 1:
         return True
@@ -19,16 +22,11 @@ def is_sorted(op, lst):
 
 # SECTION task 1
 
-# 1a
-"""
-def shellSort(A):     
-    seg_size = len(A)//2 
-    while seg_size > 0:
-        for start_i in range(seg_size):
-            jump_InsertSort(A, start_i, seg_size)
-        seg_size = seg_size // 2
-"""
+# SUBSECT 1a
 
+"""
+Insertionsosrt. Helper function for Shellsort below.
+"""
 def jump_InsertSort(A, start, step):
     for i in range(start+step, len(A), step):
         value = A[i]
@@ -38,21 +36,29 @@ def jump_InsertSort(A, start, step):
             j = j - step
         A[j] = value
 
+"""
+Sorts a list using Shellsort.
+For the segment-size, prime numbers are used instead of half of the size of the list.
 
+"""
 def shellsort(lst):
     magic = (1391376, 463792, 198768, 86961, 33936, 13776, 4592, 1968, 861, 336, 112, 48, 21, 7, 3, 1)
     magic_idx = 0
     
+    # Find biggest number in the magic list that can work for the algorithm.
     for i in range(0, len(magic)):
         magic_idx = i
         if magic[i] <= len(lst):
             break
-        
     magic_idx -= 1
+
+    # Check if the list is to big
     if magic_idx < 0:
         print("list to big.")
         return
     
+    # Iterate through the numbers in the magic list, 
+    # starting at the index calculated above.
     while magic_idx < len(magic):
         for start_i in range(magic[magic_idx]):
             jump_InsertSort(lst, start_i, magic[magic_idx])
@@ -60,8 +66,10 @@ def shellsort(lst):
     
     return lst
 
-
 """
+Eine alternative Implementation, die besser ist als die obige,
+sich aber aber nicht an der Vorlesung orientiert:
+
 def shellsort(lst):
     magic = (1391376, 463792, 198768, 86961, 33936, 13776, 4592, 1968, 861, 336, 112, 48, 21, 7, 3, 1)
     
@@ -77,55 +85,14 @@ def shellsort(lst):
     return lst
 """
 
-def test_shellsort_with_magic():
-    print("Testing shellsort with magic-list.")
-    
-    l = []
-    print("  Size   ", 0)
-    random.shuffle(l)
-    shellsort(l)
-    print("  Sorted?", is_sorted(operator.le, l))
 
-    l = list(range(0, 1))
-    print("  Size   ", len(l))
-    random.shuffle(l)
-    shellsort(l)
-    print("  Sorted?", is_sorted(operator.le, l))
 
-    l = list(range(0, 10))
-    print("  Size   ", len(l))
-    random.shuffle(l)
-    shellsort(l)
-    print("  Sorted?", is_sorted(operator.le, l))
+# SUBSECT 1b
 
-    l = list(range(0, 100))
-    print("  Size   ", len(l))
-    random.shuffle(l)
-    shellsort(l)
-    print("  Sorted?", is_sorted(operator.le, l))
-
-    l = list(range(0, 1000))
-    print("  Size   ", len(l))
-    random.shuffle(l)
-    shellsort(l)
-    print("  Sorted?", is_sorted(operator.le, l))
-
-    l = list(range(0, 10000))
-    print("  Size   ", len(l))
-    random.shuffle(l)
-    shellsort(l)
-    print("  Sorted?", is_sorted(operator.le, l))
-
-    l = list(range(0, 100000))
-    print("  Size   ", len(l))
-    random.shuffle(l)
-    shellsort(l)
-    print("  Sorted?", is_sorted(operator.le, l))
-
-# 1b
-# TODO Schreiben Sie eine Variante der Funktion, die die Anzahl der Vergleichs-Operationen zählt und als Rückgabewert zurückgibt. 
-
-def jump_InsertSort(A, start, step):
+"""
+Performs insertionsort and counts comparison operations (<, >, <=, >=)
+"""
+def jump_insert_sort_ops(A, start, step):
     cmp_ops = 0
     for i in range(start+step, len(A), step):
         value = A[i]
@@ -137,59 +104,28 @@ def jump_InsertSort(A, start, step):
         A[j] = value
     return cmp_ops
     
-
+"""
+Performs Shellsort and counts comparison operations (<, >, <=, >=).
+Internally, insertionsort is used. The comparisons performed in insertionsort 
+are counted in the total as well.
+"""
 def shellsort_ops(A):
     cmp_ops = 0
     seg_size = len(A)//2 
     while seg_size > 0:
         cmp_ops += 1
         for start_i in range(seg_size):
-            cmp_ops += jump_InsertSort(A, start_i, seg_size)
+            cmp_ops += jump_insert_sort_ops(A, start_i, seg_size)
         seg_size = seg_size // 2
     return cmp_ops
+
+# SUBSECT 1c
+
 """
-l = []
-random.shuffle(l)
-cmps = shellsort_ops(l)
-print("Size:", len(l), "Compares:", cmps)
-
-l = [17]
-random.shuffle(l)
-cmps = shellsort_ops(l)
-print("Size:", len(l), "Compares:", cmps)
-
-l = list(range(0, 10))
-random.shuffle(l)
-cmps = shellsort_ops(l)
-print("Size:", len(l), "Compares:", cmps)
-
-l = list(range(0, 100))
-random.shuffle(l)
-cmps = shellsort_ops(l)
-print("Size:", len(l), "Compares:", cmps)
-
-l = list(range(0, 1000))
-random.shuffle(l)
-cmps = shellsort_ops(l)
-print("Size:", len(l), "Compares:", cmps)
-
-l = list(range(0, 10000))
-random.shuffle(l)
-cmps = shellsort_ops(l)
-print("Size:", len(l), "Compares:", cmps)
-
-l = list(range(0, 100000))
-random.shuffle(l)
-cmps = shellsort_ops(l)
-print("Size:", len(l), "Compares:", cmps)
+Insertion sort.
+Returns the numer of comparisons that were performed while sorting the list.
+This is a helper-function for comp_shellsort_insertionsort_ops_helper(...)
 """
-#l = list(range(0, 1000000))
-#random.shuffle(l)
-#cmps = shellsort_ops(l)
-#print("Size:", len(l), "Compares:", cmps)
-
-# 1c
-# TODO Schreiben Sie ein Python-Programm, das bei gleichen Zahleneingaben die Anzahl der Vergleichs-Operation des Shellsort- und Insertsort-Algorithmus in eine tabellarische Ausgabe ausgibt. Sie sollen dabei sortierte und unsortierte Zahlen verwenden. Interessant wird, wenn die Eingabezahlen umgekehrt sortiert sind. 
 def insertion_sort_ops(lst):
     ops = 0
     for i in range(1, len(lst)):
@@ -201,13 +137,25 @@ def insertion_sort_ops(lst):
             j -= 1
         lst[j+1] = key
     return ops
-  
+
+"""
+Sorts a list using insertionsort and then shellsort and returns a Tuple of the number of operations.
+Both algorithms should work on even terms because the list is copied beforehand. The original is sorted via. insertionsort and the copy using shellsort.
+This is a helper function for comp_shellsort_insertionsort_ops(...)
+"""
 def comp_shellsort_insertionsort_ops_helper(lst):
-    l1 = lst[:]
-    is_ops = insertion_sort_ops(lst)
-    ss_ops = shellsort_ops(l1)
+    l1 = lst[:] # Copy
+    is_ops = insertion_sort_ops(lst) # Insertionsort
+    ss_ops = shellsort_ops(l1) # Shellsort
+    # Tuple containing the number of comparisons in Insertionsort and Shellsort
     return (is_ops, ss_ops)
 
+"""
+Takes a list and prints a table of the operations that 
+Insertionsort and Shellsort needed to sort the list. 
+After that, sorted, shuffled, reversed and other variants of the 
+list afte tested too.
+"""
 def comp_shellsort_insertionsort_ops(lst):
     is_ops, ss_ops = comp_shellsort_insertionsort_ops_helper(lst)
     print(ss_ops, is_ops)
@@ -236,10 +184,10 @@ def comp_shellsort_insertionsort_ops(lst):
 # TODO
 # Funktionen in task 2 sollen mit einer Heap-Struktur eine Priority-Queue simulieren.
 
-# 2a
+# SUBSECT 2a
 # TODO Zeigen, dass Heapsort nicht stabil ist
 
-# 2b
+# SUBSECT 2b
 # TODO Mit yield bei jedem Aufruf eine Nachricht mit Priority und Zeitstempel generieren
 def next_message():
     while True:
@@ -247,7 +195,7 @@ def next_message():
         timestamp = time.time()
         yield (priority, timestamp)
 
-# 2c
+# SUBSECT 2c
 
 # SUBSECT Helpers for heap-structure
 
@@ -325,13 +273,13 @@ def delete(priority_queue):
 def sort_messages(priority_queue):
     return priority_queue
 
-# 2d
+# SUBSECT 2d
 
 # TODO
 def sim_message_traffix():
     return None
 
-# 2e
+# SUBSECT 2e
 
 # TODO Complexity of next_message
 # TODO Complexity of insert
@@ -343,64 +291,33 @@ def sim_message_traffix():
 
 # SECTION task 3
 
-# 3a
+# SUBSECT 3a
 
-# TODO Definieren Sie eine Variante des Countingsort-Algorithmus aus der Vorlesung, bei dem Sie die Elemente in-Place sortieren. Ihre Countingsort Variante muss nicht stabil sein. Sie dürfen als einzigen zusätzlichen Speicher das Hilfsrarray C verwenden (siehe Vorlesungsfolien). Das Hilfsarray B darf nicht mehr verwendet werden.
-
-"""
-Vorlesung:
-def counting_sort(A, k):
-    size = len(A)
-    B = [0 for i in range(0, size)]
-    C = [0 for i in range(0, k+1)]
-
-    for j in range(0, size):
-        C[A[j]] += 1
-    for i in range(1, k+1):
-        C[i] += C[i-1]
-    for j in range(size-1, -1, -1):
-        C[A[j]] -= 1
-        B[C[A[j]]] = A[j]
-    return B
-
-Wikibooks:
-def counting_sort_in_place(array, maxval):
-    m = maxval + 1
-    count = [0] * m               # init with zeros
-    for a in array:
-        count[a] += 1             # count occurences
-    i = 0
-    for a in range(m):            # emit
-        for c in range(count[a]): # - emit 'count[a]' copies of 'a'
-            array[i] = a
-            i += 1
-    return (array,count)
-"""
 def counting_sort_in_place(lst, k):
     C = [0] * k               # k
     for a in lst:
-        C[a] += 1             # n*(1+1)
+        C[a] += 1             # n*2
     i = 0
     for a in range(k):
         for c in range(C[a]):
-            lst[i] = a        # n*k*(1+1)
+            lst[i] = a        # n*k*2
             i += 1            # n*k*1
-# 
-# Von oben zusammengezählt:
-#   k + n*(1+1) + n*k*(1+1) + n*k*1
-#   k + 2*n + 2*(k*n) + n*k
-#   k + 2*n + 3*(k*n)
-#   3*k*n
-#   O(3*k*n)
 
+# SUBSECT 3b
+
+"""
+Komplexität:
+Von oben zusammengezählt:
+  k + n*(1+1) + n*k*(1+1) + n*k*1
+  k + 2*n + 2*(k*n) + n*k
+  k + 2*n + 3*(k*n)
+  3*k*n
+  O(3*k*n)
+  
+  Ergebnis: O(k*n)
+"""
 
 # SECTION task 4
-
-"""
-Was ist der effizienteste Weg, um eine Million 32-Bit-Ganzzahlen zu sortieren?. Die Frage hat Google an Obama im Jahr 2008 gestellt. Begründen Sie Ihre Antwort.
-
-Implementieren Sie Ihre Lösung und testen Sie diese mit einer Million zufällig erzeugter 32-Bit Zahlen. Erläutern Sie Vor- und Nachteile Ihrer Lösung.
-"""
 
 """
 Als erstes fällt auf, dass für so große Mengen an ganzen Zahlen vergleichslose Sortier-Algorithmen eignen. 
@@ -410,43 +327,25 @@ Alle Algorithmen schnitten gut ab. Bei Erhöhung der Zahlenmenge auf eine Millio
 Schon bei einer Listen-Größe von einer Million Elementen im Bereich -2**28 bis 2**28 funktionierte also nur noch Pigeonholesort (auch wenn es ein paar Sekunden benötigte).
 Beim Test von vollen 32-Bit Zahlen gaben unsere Python3-Implementationen leider auf.
 
+Während das Internet Radix-Sort bevorzugt, konnte bei uns also nur Pigeonholesort die Anforderungen erfüllen.
+
+Ein Problem beim Pigeonhole Sort ist, dass er
+  1. Sehr stark von der Differenz zwischen den Werten abhängig ist
+  2. Bei sortierten Listen keinen Vorteil bietet. (Auch hier domiert 1.)
 """
 
-num_min = int(-(2**28) * 1.5)
+num_min = int(-(2**28) * 1.5) #int(-(2**28) * 1.5)
 num_max = -num_min #0x7FFFFFFF
-k = 1000000
+k = 10000000
 r = range(0, k) # range(0, 1000000)
 
-def counting_sort(A, k):
-    size = len(A)
-    B = [0 for i in range(0, size)]
-    C = [0 for i in range(0, k+1)]
-
-    for j in range(0, size):
-        C[A[j]] += 1
-    for i in range(1, k+1):
-        C[i] += C[i-1]
-    for j in range(size-1, -1, -1):
-        C[A[j]] -= 1
-        B[C[A[j]]] = A[j]
-    return B
-
 """
-lst = [random.randint(0, num_max) for _ in r]
-#t0 = time.time()
-#k = max(lst)
-#t1 = time.time()
-#print("Find max for counting sort:", t1-t0)
-
-k = r[1]
-
-t0 = time.time()
-lst = counting_sort(lst, k)
-t1 = time.time()
-print("counting_sort:", t1-t0)
-print("  Sorted?", sorted(lst))
+Pigeonhole Sort.
+Input:
+    a Multible sequential collection
+Output:
+    Sorted list (a is sorted in-place)
 """
-
 def pigeonhole_sort(a): 
     my_min = min(a) 
     my_max = max(a) 
@@ -465,6 +364,35 @@ def pigeonhole_sort(a):
             i += 1
     return a
 
+"""
+Die Zeitkomplexität des Algorithmus ist O(n+m) wobei n die Länge der Input-Liste ist und m die Differenz zwischen dem kleinsten und dem größten Wert in der Liste.
+Die Space-Komplexität ist ebenso auch O(n+m), da eine neue Liste der Größe m generiert wird.
+
+Komplexität: O(n+m)
+
+def pigeonhole_sort(a): 
+    my_min = min(a)                   # 1
+    my_max = max(a)                   # 1
+    size = my_max - my_min + 1        # 2
+    holes = [0] * size                # 2 (Time complexity O(2) aber Space O(m))
+  
+    for x in a:                     # n
+        holes[x - my_min] += 1        # n * 3 (3 operationen)
+  
+    i = 0                             # 1
+    for count in range(size):       # m
+        while holes[count] > 0:     # k
+            holes[count] -= 1         # m * k * 2
+            a[i] = count + my_min     # m * k * 2
+            i += 1                    # m * k * 1
+    return a
+    
+                                      # O(6 + 3n + 5mk)
+                                      # O(n + mk)
+                                      # O(n + m) (da m in m*k der dominierende Ausdruck ist)
+                                      # O(n+m)
+"""
+
 lst = [random.randint(num_min, num_max) for _ in r]
 t0 = time.time()
 lst = pigeonhole_sort(lst)
@@ -472,87 +400,77 @@ t1 = time.time()
 print("pigeonhole_sort:", t1-t0)
 print("  Sorted?", is_sorted(operator.le, lst))
 
-def radix_sort(a,n,maxLen):
-    bins = [[] for i in range(n)]
-    for x in range(maxLen):
-        for b in bins:
-            b.clear()
-        for y in a:
-            bins[(y//n**x)%n].append(y)
-        a=[]
-        for section in bins:
-            a.extend(section)
-    return a
+t0 = time.time()
+lst = pigeonhole_sort(lst)
+t1 = time.time()
+print("pigeonhole_sort:", t1-t0)
+print("  Sorted?", is_sorted(operator.le, lst))
 
-def radix_sort_1(A):
-    RADIX = 10
-    maxLength = False
-    tmp, placement = -1, 1
+lst = [random.randint(0, num_max) for _ in r]
+t0 = time.time()
+lst = pigeonhole_sort(lst)
+t1 = time.time()
+print("pigeonhole_sort:", t1-t0)
+print("  Sorted?", is_sorted(operator.le, lst))
+
+lst = [random.randint(num_min, 0) for _ in r]
+t0 = time.time()
+lst = pigeonhole_sort(lst)
+t1 = time.time()
+print("pigeonhole_sort:", t1-t0)
+print("  Sorted?", is_sorted(operator.le, lst))
+
+lst = [0 for _ in r]
+t0 = time.time()
+lst = pigeonhole_sort(lst)
+t1 = time.time()
+print("pigeonhole_sort:", t1-t0)
+print("  Sorted?", is_sorted(operator.le, lst))
+
+#####
+# SECTION Tests
+
+def test_shellsort_with_magic():
+    print("Testing shellsort with magic-list.")
     
-    while not maxLength:
-        buckets = [list() for _ in range(RADIX)]
-        for i in A:
-            tmp = i
-            buckets[tmp % RADIX].append(i)
-            if maxLength and tmp > 0:
-                maxLength = False
-                
-        a = 0
-        for b in range(RADIX):
-            buck = buckets[b]
-            for i in buck:
-                A[a] = i
-                a += 1
-        # move to next digit
-        placement *= RADIX
-    return A
+    l = []
+    print("  Size   ", 0)
+    random.shuffle(l)
+    shellsort(l)
+    print("  Sorted?", is_sorted(operator.le, l))
 
-def radix_sort_2(A):
-    n = len(A)
-    B = [[] for i in range(n)]
-    for i in range(n):
-        B[math.floor((A[i])*10)].append(A[i])
-    for i in range(0, n):
-        pigeonhole_sort(B[i])
-    R = []
-    for i in range(n):
-        R = R + B[i]
-    return R
+    l = list(range(0, 1))
+    print("  Size   ", len(l))
+    random.shuffle(l)
+    shellsort(l)
+    print("  Sorted?", is_sorted(operator.le, l))
 
-"""
-lst = [random.randint(num_min, num_max) for _ in r]
-t0 = time.time()
-lst = radix_sort(lst, 100, 50)
-t1 = time.time()
-print("Radix LSD (10, 5):", t1-t0)
-print("  Sorted?", is_sorted(operator.le, lst))
+    l = list(range(0, 10))
+    print("  Size   ", len(l))
+    random.shuffle(l)
+    shellsort(l)
+    print("  Sorted?", is_sorted(operator.le, l))
 
-lst = [random.randint(num_min, num_max) for _ in r]
-t0 = time.time()
-lst = radix_sort(lst, 50, 50)
-t1 = time.time()
-print("Radix LSD (10, 5):", t1-t0)
-print("  Sorted?", is_sorted(operator.le, lst))
+    l = list(range(0, 100))
+    print("  Size   ", len(l))
+    random.shuffle(l)
+    shellsort(l)
+    print("  Sorted?", is_sorted(operator.le, l))
 
-lst = [random.randint(num_min, num_max) for _ in r]
-t0 = time.time()
-lst = radix_sort(lst, 100, max(lst))
-t1 = time.time()
-print("Radix LSD (100, 10):", t1-t0)
-print("  Sorted?", is_sorted(operator.le, lst))
-"""
-"""
-lst = [random.randint(num_min, num_max) for _ in r]
-t0 = time.time()
-lst = radix_sort_2(lst)
-t1 = time.time()
-print("Radix2:", t1-t0)
-print("  Sorted?", is_sorted(operator.le, lst))
+    l = list(range(0, 1000))
+    print("  Size   ", len(l))
+    random.shuffle(l)
+    shellsort(l)
+    print("  Sorted?", is_sorted(operator.le, l))
 
-lst = [random.randint(num_min, num_max) for _ in r]
-t0 = time.time()
-lst = radix_sort_1(lst)
-t1 = time.time()
-print("Radix1:", t1-t0)
-print("  Sorted?", is_sorted(operator.le, lst))
-"""
+    l = list(range(0, 10000))
+    print("  Size   ", len(l))
+    random.shuffle(l)
+    shellsort(l)
+    print("  Sorted?", is_sorted(operator.le, l))
+
+    l = list(range(0, 100000))
+    print("  Size   ", len(l))
+    random.shuffle(l)
+    shellsort(l)
+    print("  Sorted?", is_sorted(operator.le, l))
