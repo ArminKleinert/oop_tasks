0d47b6616144d99396d6b24732115e671ccfbeae
ArminKleinert <armin.kleinert@t-online.de>
2020-06-15 19:49:27 +0200

    File for task 5

---
+++ task5/task.py
@@ -0,0 +1,39 @@
+
+# SECTION Task 1
+
+
+# SECTION Task 2
+
+
+# SECTION Task 3
+
+
+# SECTION Task 4
+
+# SUBSECT 4a
+
+
+# SUBSECT 4b
+
+
+# SECTION Task 5
+
+# SUBSECT 5a
+
+
+# SUBSECT 5b
+
+
+# SECTION 6
+
+# SUBSECT 6a
+
+
+# SUBSECT 6b
+
+
+##################################################
+# SECTION Tests                                  #
+##################################################
+
+
