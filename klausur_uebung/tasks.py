bafc3044ee5791716870896f92208f101858212c
ArminKleinert <armin.kleinert@t-online.de>
2020-08-05 19:49:28 +0200

    .

--- klausur_uebung/tasks.py
+++ klausur_uebung/tasks.py
@@ -27,3 +27,23 @@ def pow_sub(n0, n1, res):
 
 def pow_tail_rec(n0, n1):
     return pow_sub(n0, n1, n1)
+
+
+def zipWith(f, xs, ys):
+    if len(xs) == 0 or len(ys) == 0:
+        return []
+    return [f(xs[0], ys[0])] + zipWith(f, xs[1:], ys[1:])
+
+def zipWithIter(f, xs, ys):
+    output = []
+    for i in range(min(len(xs), len(ys))):
+        output.append(f(xs[0], ys[1]))
+    return output
+
+
+def fib(n, a = 0, b = 1):
+    if n == 0:
+        return a
+    if n == 1:
+        return b
+    return fib(n - 1, b, a + b)
